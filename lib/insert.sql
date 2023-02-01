afb394ed417390edef7dd71c0d31d4d02cbc9ba3
Jacob David Diehl <jakediehl17@gmail.com>
2023-02-01 15:44:42 -0700

    commit tree be9502aa5fbe90ec64103749b4f48810b7a3ad29

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 22, "M", "brown", "calm", 1),
+("Rowdy", 11, "M", "brown", "calm", 1),
+("Tabitha", 32, "F", "brown", "crazy", 1),
+("Sergeant Brown", 17, "M", "brown", "wild", 0),
+("Melissa", 10, "F", "brown", "calm", 1),
+("Grinch", 45, "M", "brown", "calm", 0),
+("Wendy", 32, "F", "brown", "calm", 1),
+(NULL, 8, "M", "brown", "nice", 1);
